0ff65a8a230b3cd6b629e84153e01ceddc7610bd
JojiAndela <42158522+JojiAndela@users.noreply.github.com>
2019-05-04 16:12:31 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'todos#index'
 
-  # post 'todos/:id/complete', to: 'todos#complete', as: :complete_todo
-  # post 'todos/:id/uncomplete', to: 'todos#uncomplete', as: :uncomplete_todo
-  get 'todos/:id/complete', to: 'todos#complete', as: :complete_todo
-  get 'todos/:id/uncomplete', to: 'todos#uncomplete', as: :uncomplete_todo
+  post 'todos/:id/complete', to: 'todos#complete', as: :complete_todo
+  post 'todos/:id/uncomplete', to: 'todos#uncomplete', as: :uncomplete_todo
 end
